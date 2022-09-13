ae07329ccb7c920637ea7194dfde3e44d282fa54
CashSorensen <112982327+CashSorensen@users.noreply.github.com>
2022-09-13 13:00:42 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+input.onButtonPressed(Button.A, function () {
+    Fr = randint(0, 1)
+    if (Fr == 0) {
+        basic.showString("Truth")
+    } else {
+        basic.showString("Dare")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Ong = randint(0, 3)
+    if (Ong == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (Ong == 2) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (Ong == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
+let Ong = 0
+let Fr = 0
+basic.showString("Truth or Dare")
+basic.forever(function () {
+	
+})
